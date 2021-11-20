7154577c8d6caa85bc10ed5232863e75fa4fd3fb
aranibar <aranibargerson28@gmail.com>
2021-11-19 21:11:51 -0500

    agregando serializers y viewsets

--- backend/icard/icard/urls.py
+++ backend/icard/icard/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from users.api.router import router_user
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -19,5 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('api/', include('users.api.router')),
+    path('api/', include(router_user.urls)),
+    
     
 ]
