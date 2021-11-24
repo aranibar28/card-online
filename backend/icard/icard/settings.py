250f38b41be17b6a8c772d6dfd72979cef2b0093
aranibar <aranibargerson28@gmail.com>
2021-11-24 16:54:37 -0500

    Agregando serializers del modelo categories

--- backend/icard/icard/settings.py
+++ backend/icard/icard/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 import datetime
 from pathlib import Path
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
     'users',
+    'categories',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -144,3 +148,5 @@ CARS_ALLOW_CREDENTIALS = True
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=120)
 }
+
+
--- backend/icard/icard/urls.py
+++ backend/icard/icard/urls.py
@@ -4,6 +4,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from users.api.router import router_user
+from categories.api.router import router_category
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -24,6 +25,6 @@ urlpatterns = [
 
     path('api/', include('users.api.router')),
     path('api/', include(router_user.urls)),
-    
+    path('api/', include(router_category.urls)),
     
 ]
