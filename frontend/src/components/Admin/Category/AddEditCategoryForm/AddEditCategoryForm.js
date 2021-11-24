988239768cd9d7a98b43a525d43df9fb7355decf
aranibar <aranibargerson28@gmail.com>
2021-11-24 18:42:43 -0500

    Agregando modal para la creacion de de categorias

---
+++ frontend/src/components/Admin/Category/AddEditCategoryForm/AddEditCategoryForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Form, Image, Button } from "semantic-ui-react";
+import "./AddEditCategoryForm.scss";
+
+export function AddEditCategoryForm() {
+  return (
+    <Form className="add-edit-category-form">
+      <Form.Input name="title" placeholder="Nombre de la categoria" />
+      <Button type="button" fluid>
+        Subir imagen
+      </Button>
+      <Button type="submit" primary fluid content="Crear" />
+    </Form>
+  );
+}
--- frontend/src/pages/Admin/CategoriesAdmin.js
+++ frontend/src/pages/Admin/CategoriesAdmin.js
@@ -1,10 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Loader } from "semantic-ui-react";
-import { HeaderPage, TableCategoryAdmin } from "../../components/Admin";
+import {
+  HeaderPage,
+  TableCategoryAdmin,
+  AddEditCategoryForm,
+} from "../../components/Admin";
 import { useCategory } from "../../hooks";
+import { ModalBasic } from "../../components/Common";
 
 export function CategoriesAdmin() {
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState(null);
+  const [contentModal, setContentModal] = useState(null);
   const { loading, categories, getCategories } = useCategory();
+  const openCloseModal = () => setShowModal((prev) => !prev);
+
+  const addCategory = () => {
+    setTitleModal("Nueva Categoría");
+    setContentModal(<AddEditCategoryForm />);
+    openCloseModal();
+  };
 
   useEffect(() => {
     getCategories();
@@ -13,7 +28,11 @@ export function CategoriesAdmin() {
 
   return (
     <div>
-      <HeaderPage title="Categorias" btnTitle="Nueva Categoría" />
+      <HeaderPage
+        title="Categorias"
+        btnTitle="Nueva Categoría"
+        btnClick={addCategory}
+      />
       {loading ? (
         <Loader active inline="centered">
           Cargando...
@@ -21,6 +40,12 @@ export function CategoriesAdmin() {
       ) : (
         <TableCategoryAdmin categories={categories} />
       )}
+      <ModalBasic
+        show={showModal}
+        onClose={openCloseModal}
+        title={titleModal}
+        children={contentModal}
+      />
     </div>
   );
 }
