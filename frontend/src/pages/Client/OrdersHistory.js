1fe6bd27da2da8c8d1580a4fb631fd4da81d9c11
aranibar <aranibargerson28@gmail.com>
2021-11-30 23:46:25 -0500

    Funcionalidad de estado de cuentas finalizados

--- frontend/src/pages/Client/OrdersHistory.js
+++ frontend/src/pages/Client/OrdersHistory.js
@@ -1,25 +1,94 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "semantic-ui-react";
 import { useParams } from "react-router-dom";
-import { useOrder, useTable } from "../../hooks";
+import { map, size, forEach } from "lodash";
+import { OrderHistoryItem } from "../../components/Client";
+import { ModalConfirm } from "../../components/Common";
+import { useOrder, useTable, usePayment } from "../../hooks";
 
 export function OrdersHistory() {
-  const { loading, orders, getOrdersByTable } = useOrder();
+  const [idTable, setIdTable] = useState(null);
+  const [showTypePayment, setShowTypePayment] = useState(false);
+  const [isRequestAccount, setIsRequestAccount] = useState(false);
+  const { loading, orders, getOrdersByTable, addPaymentToOrder } = useOrder();
   const { getTableByNumber } = useTable();
   const { tableNumber } = useParams();
-  console.log(orders);
+  const { createPayment, getPaymentByTable } = usePayment();
 
   useEffect(() => {
     (async () => {
       const table = await getTableByNumber(tableNumber);
-      const idTable = table[0].id;
-      getOrdersByTable(idTable, "", "ordering=-status,-created_at");
+      const idTableTemp = table[0].id;
+      setIdTable(idTableTemp);
+
+      getOrdersByTable(idTableTemp, "", "ordering=-status,-created_at");
     })();
     // eslint-disable-next-line
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      if (idTable) {
+        const response = await getPaymentByTable(idTable);
+        setIsRequestAccount(response);
+      }
+    })();
+    // eslint-disable-next-line
+  }, [idTable]);
+
+  const onCreatePayment = async (typePayment) => {
+    setShowTypePayment(false);
+
+    let totalPayment = 0;
+    forEach(orders, (order) => {
+      totalPayment += Number(order.product_data.price);
+    });
+    const paymentData = {
+      table: idTable,
+      totalPayment: totalPayment.toFixed(2),
+      typePayment,
+      statusPayment: "PENDING",
+    };
+    const payment = await createPayment(paymentData);
+    for await (const order of orders) {
+      await addPaymentToOrder(order.id, payment.id);
+    }
+    window.location.reload();
+  };
+
   return (
     <div>
       <h1>Historial de Pedidos</h1>
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
+        <>
+          {size(orders) > 0 && (
+            <Button
+              primary
+              fluid
+              onClick={() =>
+                size(isRequestAccount) === 0 && setShowTypePayment(true)
+              }
+            >
+              {size(isRequestAccount) > 0
+                ? "La cuenta ya esta pedida"
+                : "Pedir la cuenta"}
+            </Button>
+          )}
+          {map(orders, (order) => (
+            <OrderHistoryItem key={order.id} order={order} />
+          ))}
+        </>
+      )}
+      <ModalConfirm
+        title="Pagar con tarjeta o efectivo?"
+        show={showTypePayment}
+        onCloseText="Efectivo"
+        onClose={() => onCreatePayment("CASH")}
+        onConfirmText="Tarjeta"
+        onConfirm={() => onCreatePayment("CARD")}
+      />
     </div>
   );
 }
